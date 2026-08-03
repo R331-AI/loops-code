ede77054192083ffa4ec911dd82ca66b086a9363
Tumhara Name <tumharaemail@example.com>
2026-08-03 18:35:53 +0530

    Added loop_prime and reverse_string

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+a = input("enter your string:")
+
+b = "" 
+
+for i in range(len(a)-1,-1,-1):
+    
+    b = b + a[i]
+
+if b==a:
+    print("your string is palindrome")
+
+else:
+    print("its not a pallindrome ")
+
